c3edfe384510febd96f20de175985e116d7e7614
pokasireesha2303 <pokasireesha@gmail.com>
2026-06-30 17:16:26 +0530

    Add auth routes for signup and login, mount in server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const stockRoutes = require('./routes/stockRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/stocks', stockRoutes);
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
